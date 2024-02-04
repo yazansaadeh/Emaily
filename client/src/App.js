595ef3b37a0b412e4f33888fbd9fd2b23eb77615
Yazan <yazansaada0@gmail.com>
2024-02-05 00:22:41 +0300

    add a store folder for redux/toolkit and create oathSlice

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,22 @@
+import { Route, BrowserRouter } from "react-router-dom";
+
+const Header = () => <h1>Header</h1>;
+const Landing = () => <h1>Landing</h1>;
+const SurveyNew = () => <h1>SurveyNew</h1>;
+const Dashboard = () => <h1>Dashboard</h1>;
+
 function App() {
-  return <div>Hi There</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing}></Route>
+          <Route exact path="/surveys" component={Dashboard}></Route>
+          <Route path="/surveys/new" component={SurveyNew}></Route>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
